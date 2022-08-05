cfc1ef81f2d42cc9507fb15a7bac360f8870d761
sagid.veliev <dagfarid73@gmail.com>
2022-08-05 20:08:21 +0400

    Added computed and styles for sizes and colors

--- components/Cart.vue
+++ components/Cart.vue
@@ -20,20 +20,25 @@
                     <div class="cart_description_wrap_color">
                         <div class="cart_description_wrap_color_label-color">Color:</div>
                         <div class="cart_description_wrap_color_colors">                     
-                            <div class="cart_description_wrap_color_colors_single-color">
-                                as
-                            </div>
-                            <div class="cart_description_wrap_color_colors_single-color">
-                                d
-                            </div>
-                            <div class="cart_description_wrap_color_colors_single-color">
-                               sa 
+                            <div v-for="(color, index) in availableColors.values" class="cart_description_wrap_color_colors_single-color">
+                                <nuxt-img
+                                    class="cart_description_wrap_color_colors_single-color_image"
+                                    :alt="`${color.label}`"
+                                    :src="`~/assets/images/conf/${color.label.toLowerCase()}.png`"
+                                    loading="lazy"
+                                />
                             </div>
                         </div>
                     </div>
                     <div class="cart_description_wrap_size">
                         <div class="cart_description_wrap_size_label-size">Size:</div>
-                        <div class="cart_description_wrap_size_sizes"></div>
+                        <div class="cart_description_wrap_size_sizes">
+                            <div v-for="(size, index) in availableSizes.values" class="cart_description_wrap_size_sizes_single-size">
+                                <div class="cart_description_wrap_size_sizes_single-size_size">
+                                    {{size.label}}
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     
                 </template>
@@ -58,6 +63,12 @@ export default {
     computed: {
         fixedPrice() {
             return this.price.toFixed(2);
+        },
+        availableColors() {
+            return this.configurable[0];
+        },
+        availableSizes() {
+            return this.configurable[1];
         }
     },
     methods: {
@@ -68,7 +79,6 @@ export default {
 }
 </script>
 <style lang="postcss">
-
     .cart {
         display: flex;
         flex-direction: column;
@@ -101,7 +111,7 @@ export default {
             &_wrap {
                 display: flex;
                 flex-direction: column;
-                flex: 0.95;
+                flex-basis: 95%;
                 height: 100%;
                 padding: -10px;
                 &_name {
@@ -118,37 +128,90 @@ export default {
                 }
                 &_color {
                     display: flex;
+                    flex-basis: 20%;
                     flex-direction: row;
-                    border: 1px solid red;
-                    min-height: 30px;
+                    box-sizing: border-box;
+                    min-height: 50px;
                     height: 100%;
                     &_label-color {
                         display: flex;
                         flex-basis: 20%;
                         align-items: center;
                     }
-                    &_colors{
+                    &_colors {
                         flex-basis: 80%;
                         display: flex;
                         flex-direction: row;
+                        justify-content: flex-end;
                         flex-wrap: wrap;
-                        justify-content: space-between;
                         &_single-color {
-                            display: flex;
-                            flex-direction: row;
+                            position: relative;
                             flex-basis: 25%;
+                            height: 45px;
+                            overflow: hidden;
                             margin: 1px;
-                            border:1px solid blue;
+                            &_image {
+                                background-color: rgba(235, 235, 235);
+                                border-radius: 5px;
+                                position: absolute;
+                                top: 0;
+                                bottom: 0;
+                                left: 0;
+                                right: 0;
+                                max-width: 100%;
+                                max-height: 100%;
+                                margin: auto;
+                                &:hover {
+                                    background-color: rgba(235, 115, 3, 0.5);
+                                }
+                            }
                         }
                     }
                 }
-                &_size {
+                 &_size {
                     display: flex;
+                    flex-basis: 20%;
                     flex-direction: row;
-                    border: 1px solid blue;
+                    box-sizing: border-box;
+                    min-height: 50px;
+                    height: 100%;
                     &_label-size {
+                        display: flex;
                         flex-basis: 20%;
-                        background-color: green;
+                        align-items: center;
+                    }
+                    &_sizes {
+                        flex-basis: 80%;
+                        display: flex;
+                        flex-direction: row;
+                        justify-content: flex-end;
+                        flex-wrap: wrap;
+                        &_single-size {
+                            background-color: rgba(235, 235, 235);
+                            position: relative;
+                            flex-basis: 25%;
+                            height: 45px;
+                            overflow: hidden;
+                            margin: 1px;
+                            border-radius: 5px;
+                            &_size {
+                                display: flex;
+                                justify-content: center;
+                                align-items: center;
+                                position: absolute;
+                                top: 0;
+                                bottom: 0;
+                                left: 0;
+                                right: 0;
+                                max-width: 100%;
+                                max-height: 100%;
+                                margin: auto;
+                                &:hover {
+                                    background-color: rgb(235, 115, 3);
+                                    color: white;
+                                }
+                            }
+                        }
                     }
                 }
                 &_price {
@@ -175,72 +238,6 @@ export default {
             }
         }
     }
-
-    /* .cart {
-        display: flex;
-        flex-direction: column;
-        width: 23%;
-        border-radius: 5px;
-        margin: 1%;
-        box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0,0,0,0.22);
-        &_photo {
-            height: 250px;
-            position: relative;
-            overflow: hidden;
-            &_image {
-                position: absolute;
-                top: 0;
-                bottom: 0;
-                left: 0;
-                right: 0;
-                max-width: 100%;
-                max-height: 100%;
-                margin: auto;
-            }
-        }
-        &_description {
-            flex-grow: 1;
-            background-color: rgb(248, 248, 248);
-            border-radius: 0 0 5px 5px;
-            &_wrap {
-                margin: 10px;
-                &_name {
-                    display: flex;
-                    font-weight: 600;
-                    min-height: 25%;
-                    & p:hover {
-                        color: rgb(235, 115, 3);
-                    }
-                }
-                &_brand {
-                    min-height: 25%;
-                    opacity: 0.7;
-                }
-                &_price {
-                    display:flex;
-                    justify-content: space-between;
-                    align-items: flex-end;
-                    min-height: 25%;
-                    font-weight: bold;
-                    font-size: 22px;
-                    color: rgb(235, 115, 3);
-                    &_btn {
-                        border: rgb(235, 115, 3);
-                        background-color: rgb(248, 129, 18);
-                        font-size: 16px;
-                        border-radius: 5px;
-                        padding: 5px 10px;
-                        color: white;
-                        transition: 0.2s;
-                        &:hover {
-                            background-color: rgb(235, 115, 3);
-                            cursor: pointer;
-                        }
-                    }
-                }
-            }
-        }
-    } */
 
     @media screen and (max-width: 1280px) { 
         .cart {
--- pages/index.vue
+++ pages/index.vue
@@ -1,81 +1,85 @@
 <template>
   <div>
-    <Navbar class="navbar_fixed" :quantity="quantity"/>
+    <Navbar class="navbar_fixed" :quantity="quantity" />
     <div class="content">
-        <Brands @select="selectBrand" @reset="resetBrands" class="content_filter" />
-        <Container :selected-brand="selectedBrand">
-          <template v-slot:products>
-            <Cart 
-                v-for="(product, index) in filteredProducts"
-                :key="index"
-                :name="product.title"
-                :brand="product.brand"
-                :price="product.regular_price.value"
-                :currency="product.regular_price.currency"
-                :configurable="product.configurable_options"
-                :path="product.image"
-                @add-to-basket="addToBasket(product)"
-            />
-          </template>
-        </Container>
-      </div>  
+      <Brands
+        class="content_filter"
+        @select="selectBrand"
+        @reset="resetBrands"
+      />
+      <Container :selected-brand="selectedBrand">
+        <template v-slot:products>
+          <Cart
+            v-for="(product, index) in filteredProducts"
+            :key="index"
+            :name="product.title"
+            :brand="product.brand"
+            :price="product.regular_price.value"
+            :currency="product.regular_price.currency"
+            :configurable="product.configurable_options"
+            :path="product.image"
+            @add-to-basket="addToBasket(product)"
+          />
+        </template>
+      </Container>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from "vuex";
 export default {
-    name: "IndexPage",
-      data() {
-      return {
-        sortedProducts: [],
-        selectedBrand: "",
-        quantity: null
+  name: "IndexPage",
+  data() {
+    return {
+      sortedProducts: [],
+      selectedBrand: "",
+      quantity: null,
+    };
+  },
+  async fetch() {
+    await this.$store.dispatch("GET_PRODUCTS");
+  },
+  computed: {
+    ...mapGetters(["PRODUCTS"]),
+    filteredProducts() {
+      if (!this.sortedProducts.length) {
+        return this.PRODUCTS;
       }
+      return this.sortedProducts;
     },
-    async fetch() {
-        await this.$store.dispatch("GET_PRODUCTS");
+  },
+  methods: {
+    selectBrand(brand) {
+      this.sortedProducts = [];
+      this.selectedBrand = brand;
+      this.sortedProducts = this.PRODUCTS.filter(
+        (item) => item.brand === brand
+      );
+      return this.sortedProducts;
     },
-    computed: {
-      ...mapGetters([
-        "PRODUCTS"  
-      ]),
-      filteredProducts() {
-        if (!this.sortedProducts.length) {
-          return this.PRODUCTS;
-        }
-        return this.sortedProducts;
-      }
+    resetBrands(brand) {
+      this.selectedBrand = brand;
+      this.selectBrand(brand);
     },
-    methods: {
-      selectBrand(brand) {
-        this.sortedProducts = [];
-        this.selectedBrand = brand;
-        this.sortedProducts = this.PRODUCTS.filter(item => item.brand === brand);
-        return this.sortedProducts;
-      },
-      resetBrands(brand) {
-        this.selectedBrand = brand;
-        this.selectBrand(brand);
-      },
-      addToBasket(product) {
-        this.quantity++;
-        console.log(product);
-      }
-    }
-}
-
+    addToBasket(product) {
+      this.quantity++;
+      console.log(product);
+    },
+  },
+};
 </script>
 
 <style lang="postcss" scoped>
-    .content {
-      display: flex;
-      flex-direction: row;
-      border-radius: 5px;
-      margin-top: 25px;
-      height: auto;
-      &_filter {
-        margin-right: 25px;
-      }
-    }
+.content {
+  display: flex;
+  flex-direction: row;
+  border-radius: 5px;
+  margin-top: 25px;
+  height: auto;
+
+  &_filter {
+    margin-right: 25px;
+  }
+}
 </style>
